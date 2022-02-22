f258516e99ae2e5eccc11fb26801a75984e72cdf
dwfakhre <gf_derafa@esi.dz>
2022-02-22 11:27:41 +0100

    front additions

--- back_end/index.js
+++ back_end/index.js
@@ -5,8 +5,8 @@ import cors from "cors";
 import sessions from "express-session"
 import roomRoutes from "./routes/room-path.js"
 import welcomeRoutes from "./routes/welcome.js"
-
-const app = express();
+import cookieParser from "cookie-parser";
+let app = express();
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
