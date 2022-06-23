5deb7547baa5222a102ffe75c71d4c36a6908ac3
dwfakhre <60073951+dwfakhre@users.noreply.github.com>
2022-06-23 17:17:09 +0100

    spotify api 2

--- spotify_controller/index.js
+++ spotify_controller/index.js
@@ -1,33 +1,20 @@
 import express from "express";
 import bodyParser from "body-parser";
-import mongoose from "mongoose";
+
 import cors from "cors";
-import sessions from "express-session";
-import roomRoutes from "./routes/room-path.js";
+import SpotifyRoutes from "./routes/Spotify"
 import welcomeRoutes from "./routes/welcome.js";
 import cookieParser from "cookie-parser";
-import crypto from "crypto";
-import uuid from "node-uuid";
+
 let app = express();
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
-const oneDay = 1000 * 60 * 60 * 24;
+
 
 //session middleware
-app.use(
-  sessions({
-    secret: "thisismysecrctekeyfhrgfgrfrty84fwir767",
-    genid: function (req) {
-      return uuid.v1();
-    },
-    saveUninitialized: true,
-    cookie: { maxAge: oneDay },
-    resave: false,
-    room_code: "one",
-  })
-);
+
 
 app.use(cookieParser());
 
