c78fb8f9f7d8709437300099a90772e7b025b5a6
dwfakhre <gf_derafa@esi.dz>
2022-10-29 20:40:57 +0100

    some stuff

--- front_end/src/components/Room/Room.js
+++ front_end/src/components/Room/Room.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { useParams } from "react-router-dom";
 import { useState, useEffect } from "react";
 import { room_infos } from "../../api";
@@ -14,13 +14,28 @@ export default function Room() {
   });
 
   const { code } = useParams();
-    return <h1>Welcome to this Room { code }</h1>;
-};
+
+  useEffect(() => {
+    get_room_infos(code);
+  }, [code]);
+
+  const get_room_infos = async () => {
+    console.log();
+    const res = await room_infos(code);
+    console.log(res);
+    setRoom({
+      room_code: res.code,
+      name: res.name,
+      votes: res.votes_to_skip,
+      isHost: res.isHost,
+      host: res.host,
+      guest_can_pause: res.guest_can_pause,
+    });
+  };
 
   return (
     <h1>
       Welcome to this Room {Room.name} {Room.host}
     </h1>
-    
   );
 }
